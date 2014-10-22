585c65bfc9bb56b6f279a27095cbc11420233218
Atsushi, Mori <ki4070ma@gmail.com>
2014-10-22 22:45:14 +0900

    Modifed test (Test passed: GREEN)

--- src/tdd/FizzBuzz.java
+++ src/tdd/FizzBuzz.java
@@ -3,7 +3,7 @@ package tdd;
 public class FizzBuzz {
 
 	public String say(int i) {
-		if( i == 15 ) return "FizzBuzz";
+		if( i%5 == 0 && i%3 == 0 ) return "FizzBuzz";
 		if( i%5 == 0 ) return "Buzz";
 		return "Fizz";
 	}
