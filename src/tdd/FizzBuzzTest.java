e8fde2b9319765fe08d213ac0b08406baef92004
Atsushi, Mori <ki4070ma@gmail.com>
2014-10-22 22:38:42 +0900

    Refactoring test code

--- src/tdd/FizzBuzzTest.java
+++ src/tdd/FizzBuzzTest.java
@@ -1,23 +1,28 @@
 package tdd;
 
+import org.junit.Before;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
-
 import static org.hamcrest.core.Is.*;
 import static org.hamcrest.core.IsNot.*;
 import static org.hamcrest.core.IsNull.*;
 
 public class FizzBuzzTest {
+	FizzBuzz fizzbuzz;
+
+	@Before
+	public void setUp() {
+		fizzbuzz = new FizzBuzz();
+	}
+
 	@Test
-	public void ”š‚ª3‚Ì”{”‚ÌFizz‚Æ•Ô‚·() {
-		FizzBuzz fizzbuzz = new FizzBuzz();
+	public void ”š‚ª3‚Ì”{”‚ÌFizz‚ğ•Ô‚·() {
 		assertThat(fizzbuzz.say(3), is("Fizz"));
 	}
 
 	@Test
 	public void ”š‚ª5‚Ì”{”‚ÌBuzz‚ğ•Ô‚·() {
-		FizzBuzz fizzbuzz = new FizzBuzz();
 		assertThat(fizzbuzz.say(5), is("Buzz"));
 	}
 }
