44e505a48453edaf97eceeb1055b87c37f656020
Atsushi, Mori <ki4070ma@gmail.com>
2014-10-22 22:17:48 +0900

    TDD initial commit

---
+++ src/tdd/FizzBuzzTest.java
@@ -0,0 +1,16 @@
+package tdd;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+import static org.hamcrest.core.Is.*;
+import static org.hamcrest.core.IsNot.*;
+import static org.hamcrest.core.IsNull.*;
+
+public class FizzBuzzTest {
+	@Test
+	public void test() {
+		fail("‚Ü‚¾ŽÀ‘•‚³‚ê‚Ä‚¢‚Ü‚¹‚ñ");
+	}
+}
