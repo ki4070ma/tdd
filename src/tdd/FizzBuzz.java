c85563b4a8d298cd0df51d313598d6a3ad1d9296
Atsushi, Mori <ki4070ma@gmail.com>
2014-10-22 22:51:22 +0900

    Modified say method (Test passed: GREEN)

--- src/tdd/FizzBuzz.java
+++ src/tdd/FizzBuzz.java
@@ -4,14 +4,15 @@ public class FizzBuzz {
 
 	public String say(int i) {
 		boolean isNumMod5Equals0 = (i%5 == 0);
-		if( i == 2 ) { return "2"; }
-
 		if( isNumMod5Equals0 && i%3 == 0 ) {
 			return "FizzBuzz";
 		}
 		if( isNumMod5Equals0 ) {
 			return "Buzz";
 		}
-		return "Fizz";
+		if( i%3 == 0 ) {
+			return "Fizz";
+		}
+		return String.valueOf(i);
 	}
 }
