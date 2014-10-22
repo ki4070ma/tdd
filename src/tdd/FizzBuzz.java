17a78d5d4dc02bd4185422fe70e39536da33e290
Atsushi, Mori <ki4070ma@gmail.com>
2014-10-22 22:46:25 +0900

    Refactoring say method (REFACTORING)

--- src/tdd/FizzBuzz.java
+++ src/tdd/FizzBuzz.java
@@ -3,8 +3,14 @@ package tdd;
 public class FizzBuzz {
 
 	public String say(int i) {
-		if( i%5 == 0 && i%3 == 0 ) return "FizzBuzz";
-		if( i%5 == 0 ) return "Buzz";
+		boolean isNumMod5Equals0 = (i%5 == 0);
+
+		if( isNumMod5Equals0 && i%3 == 0 ) {
+			return "FizzBuzz";
+		}
+		if( isNumMod5Equals0 ) {
+			return "Buzz";
+		}
 		return "Fizz";
 	}
 }
