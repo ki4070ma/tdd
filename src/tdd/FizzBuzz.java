625c17d57c5380c90ee26b16e9e3a12a8a01fb82
Atsushi, Mori <ki4070ma@gmail.com>
2014-10-22 22:54:22 +0900

    Modified say method (Test passed: GREEN)

--- src/tdd/FizzBuzz.java
+++ src/tdd/FizzBuzz.java
@@ -2,7 +2,10 @@ package tdd;
 
 public class FizzBuzz {
 
+
 	public String say(int num) {
+		if( num <= 0 ) throw new IllegalArgumentException();
+
 		boolean isNumMod5Equals0 = (num%5 == 0);
 		boolean isNumMod3Equals0 = (num%3 == 0);
 		if( isNumMod5Equals0 && isNumMod3Equals0) {
